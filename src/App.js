0320ffca2c4daf40873fab1a0af5fadcb4b85b2c
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-03-12 05:03:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Upload } from 'antd';
+import './App.css';
+
+const App = () => (
+	<>
+		<Upload>
+			<Button>Click to Upload</Button>
+		</Upload>
+	</>
+);
+
+export default App;
